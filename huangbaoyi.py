e6a778411c1286a9929ed0ce2f747dcd81d5b5c1
Martin-Zcreate <80252212+Martin-Zcreate@users.noreply.github.com>
2024-05-31 17:40:22 +0800

    Update huangbaoyi.py

--- huangbaoyi.py
+++ huangbaoyi.py
@@ -1,13 +1,39 @@
 import streamlit as st
+import pandas as pd
 from openai import OpenAI
+import os
 
 st.title("AI")
+
+def output():
+    for i in  range(len(st.session_state['df'])):
+        st.chat_message("user").write(st.session_state['df'].loc[i,"用户"])
+        st.chat_message("AI").write(st.session_state['df'].loc[i,"AI"])
+        st.session_state["history"]+="user:"+st.session_state['df'].loc[i,"用户"]+"\n"
+        st.session_state["history"]+="system:"+st.session_state['df'].loc[i,"AI"]+"\n"
+    
+    
 if 'history' not in st.session_state:
     st.session_state["history"]=''
 if"prompt"not in st.session_state:
     st.session_state["prompt"]=[]
-    
-    
+if "file" not in st.session_state:
+    st.session_state["file"]='ai聊天记录.csv'
+
+if "df" not in st.session_state:
+    st.session_state["df"]=[]
+if"create" not in st.session_state:
+    st.session_state["create"]=False
+if  st.session_state["create"]== False:
+    if os.path.exists(st.session_state["file"]):
+        st.session_state["df"]=pd.read_csv(st.session_state["file"])
+        
+        
+    else:
+        st.session_state["df"].to_csv(st.session_state["file"],index=False)
+    st.session_state["create"]== True
+        
+        
 
 
 def ai(prompt,temperature):
@@ -32,23 +58,26 @@ def ai(prompt,temperature):
         r+=i.choices[0].delta.content
         ai_empty.write(r)
     return r
-    
-
-prompt=st.chat_input("你有什么疑问,都可以问我啊!")
+st.session_state["history"]=''    
+output()
+prompt=st.chat_input("请输入")
 if prompt is not None:
-    for i in st.session_state['prompt']:
-        st.chat_message("user").write(i[0])
-        st.chat_message("AI").write(i[1])
+    
         
         
         
     st.chat_message("user").write(prompt)
     
     
-    st.session_state["history"]+="user:"+prompt+"\n"
+    
     
     r=ai(st.session_state["history"],1.25)
-    st.session_state["prompt"]+=[[prompt,r]]
+    st.session_state["prompt"]=[[prompt,r]]
+    new_df=pd.DataFrame(st.session_state["prompt"],columns=["用户","AI"])
+    st.session_state["df"]=pd.concat([st.session_state["df"],new_df],ignore_index=True)
+    st.session_state["df"].to_csv(st.session_state["file"],index=False)
     
-    st.session_state["history"]+="system:"+r+"\n"
+    
+    
+    
 
