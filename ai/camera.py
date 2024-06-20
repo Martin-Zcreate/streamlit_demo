e0d16ff953c2cd3cd0e7958d9f3c76baeb0c2287
Martin-Zcreate <80252212+Martin-Zcreate@users.noreply.github.com>
2024-06-20 17:18:14 +0800

    Update camera.py

--- ai/camera.py
+++ ai/camera.py
@@ -1,9 +1,10 @@
-import streamlit as st
+import requests
+import base64
+from openai import OpenAI
+import streamlit as st 
 from PIL import Image
 import numpy as np
-from openai import OpenAI
 
-img_file_buffer = st.camera_input("点击拍照1")
 
 def ai(prompt,temperature,pro):
     r=''
@@ -25,14 +26,26 @@ def ai(prompt,temperature,pro):
         r+=i.choices[0].delta.content
         ai_empty.write(r)
 
-
+pro = """
+你是一名专业的高中语文老师,你可以辅导学生完成作业,给出思路过程,并给出答案。
+"""
+s=''
+request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
+# 二进制方式打开图片文件
+img_file_buffer = st.camera_input("点击拍照")
 
 if img_file_buffer is not None:
     img = Image.open(img_file_buffer)
-    img_array = np.array(img)
-    ai("分析这个图像显示的内容",1,"你是一个图像分析师")
-    #st.write(type(img_array))
-    #st.write(img_array.shape)
-    
-    
-    
+
+# f = open('C:/Users/z/streamlitdemo/123.png', 'rb')
+img = base64.b64encode(img.read())
+
+params = {"image":img}
+access_token = '24.2c2db58f2d82deb5596706316b5342a8.2592000.1721465622.282335-84920272'
+request_url = request_url + "?access_token=" + access_token
+headers = {'content-type': 'application/x-www-form-urlencoded'}
+response = requests.post(request_url, data=params, headers=headers)
+if response:
+    for i in response.json()['words_result']:
+        s+=i['words']
+    ai(s,pro,1)
