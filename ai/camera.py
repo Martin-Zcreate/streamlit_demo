0618fd4e95ecd73be8ed4a657ba527d5dacc5a6b
Martin-Zcreate <80252212+Martin-Zcreate@users.noreply.github.com>
2024-06-20 17:27:06 +0800

    Update camera.py

--- ai/camera.py
+++ ai/camera.py
@@ -33,19 +33,18 @@ s=''
 request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
 # 二进制方式打开图片文件
 img_file_buffer = st.camera_input("点击拍照")
-
 if img_file_buffer is not None:
-    img = Image.open(img_file_buffer)
+    img = Image.open(img_file_buffer).tobytes()
 
-# f = open('C:/Users/z/streamlitdemo/123.png', 'rb')
-img = base64.b64encode(img.read())
-
-params = {"image":img}
-access_token = '24.2c2db58f2d82deb5596706316b5342a8.2592000.1721465622.282335-84920272'
-request_url = request_url + "?access_token=" + access_token
-headers = {'content-type': 'application/x-www-form-urlencoded'}
-response = requests.post(request_url, data=params, headers=headers)
-if response:
-    for i in response.json()['words_result']:
-        s+=i['words']
-    ai(s,pro,1)
+    # f = open('C:/Users/z/streamlitdemo/123.png', 'rb')
+    # img = base64.b64encode(imag.read())
+    
+    params = {"image":img}
+    access_token = '24.2c2db58f2d82deb5596706316b5342a8.2592000.1721465622.282335-84920272'
+    request_url = request_url + "?access_token=" + access_token
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    response = requests.post(request_url, data=params, headers=headers)
+    if response:
+        for i in response.json()['words_result']:
+            s+=i['words']
+        ai(s,pro,1)
