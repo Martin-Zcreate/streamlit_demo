e88c51bcc14e5b39fae5cfd1082f4096a46c49a7
Martin-Zcreate <80252212+Martin-Zcreate@users.noreply.github.com>
2024-06-20 20:34:56 +0800

    Update camera.py

--- ai/camera.py
+++ ai/camera.py
@@ -35,9 +35,10 @@ request_url = "https://aip.baidubce.com/rest/2.0/ocr/v1/accurate_basic"
 img_file_buffer = st.camera_input("点击拍照")
 if img_file_buffer is not None:
     imag = Image.open(img_file_buffer)
+    # image.save('123.png', 'PNG')
 
-    # f = open('C:/Users/z/streamlitdemo/123.png', 'rb')
-    # img = base64.b64encode(imag.read())
+    # f = open('123.png', 'rb')
+    img = base64.b64encode(imag.read())
     
     params = {"image":img}
     access_token = '24.2c2db58f2d82deb5596706316b5342a8.2592000.1721465622.282335-84920272'
