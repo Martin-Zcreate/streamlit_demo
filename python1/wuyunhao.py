91fdbe2905656fc685f7c9da9e0418d2cd775713
Martin-Zcreate <80252212+Martin-Zcreate@users.noreply.github.com>
2024-08-27 14:42:20 +0800

    Create wuyunhao.py

---
+++ python1/wuyunhao.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("吴云浩的个人网站🤡")
+
+st.title("音乐✨")
+m=st.selectbox("选择音乐",("夜曲","少年","天空之城","夜空中最亮的星"))
+a=open(f"music1/{m}.mp3","rb").read()
+st.audio(a)
+st.image(f"picture1/{m}.jpg")
+
+st.title("电影🎦")
+movie=st.multiselect("选择电影",("飞驰人生","流浪地球"))
+
+for i in movie:
+    st.image(f"picture1/{i}.jpg")
