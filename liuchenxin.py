dc7ee267922ac57ac272e6e3320f46ebf3f6a000
Martin-Zcreate <80252212+Martin-Zcreate@users.noreply.github.com>
2025-01-05 11:30:38 +0800

    Update liuchenxin.py

--- liuchenxin.py
+++ liuchenxin.py
@@ -4,15 +4,14 @@ import streamlit as st
 def ai(prompt):
     r = ""
 
-    client = OpenAI(api_key="sk-37cf4872e42446dc97cd04c694c09a10", 
+    client = OpenAI(api_key="sk-bf811718c8164f50ad0861059db1aff7", 
                     base_url="https://api.deepseek.com")  
     
     response = client.chat.completions.create(
         model="deepseek-chat",
         messages=[
             {"role": "system", "content": """你的创造者是万载县外国语学校七6班刘晨鑫,
-             请使用作家的身份写小说,你叫咸鱼梦想家
-             """},
+             请使用中学专业老师的身份出题,你叫咸鱼梦想家"""},
             {"role": "user", "content": prompt},
         ],
         stream=True
@@ -25,7 +24,7 @@ def ai(prompt):
     return r
 
 st.set_page_config(page_title="刘晨鑫的AI",
-                   layout="wide",
+                   layout="centered",
                    page_icon="🤡🤡🤣🤣"
                    )
 
@@ -37,6 +36,38 @@ if "h" not in st.session_state:
     st.session_state["h"] = ""
     ai("你是谁?")
     
+p = st.chat_input("请输入问题")  
+with st.sidebar:
+    p1 = st.text_area("输入")
+    if st.button("做数学题"):
+        p=f"""你是一个专业的中学数学老师,
+        你出一道(p1)数学题,
+        用户来回答,再判断用户是否答对
+        然后再出一道,循环"""
+    if st.button("做语文题"):
+        p=f"""你是一个专业的中学语文老师,
+        你出一道古诗词的填空题,填一句
+        用户来回答,再判断用户是否答对
+        然后再出一道,循环"""
+    if st.button("做英语题"):
+        p=f"""你是一个专业的中学英语老师,
+        你出一道英语的选择题,出一道
+        用户来回答,再判断用户是否答对
+        然后再出一道,循环"""
+    if st.button("写小说和续写小说"):
+         p=f"""你是一个专业的作家,
+         用户出小说名,
+         AI来回答,
+         然后用户再出一道,循环"""
+    if st.button("写作文"):
+        p=f"""你是一个专业的中学作文老师,
+        作文要求:{p1}
+        请写出作文思路,详细讲解作文的写法,
+        不出现人名,有范文
+    """
+   
+    
+    
 st.session_state["h"]=""
 for i in st.session_state["p"]:
     st.chat_message("user").write(i[0])
@@ -45,7 +76,7 @@ for i in st.session_state["p"]:
     st.session_state["h"]+="system"+i[1]+"\n"
         
         
-p = st.chat_input("请输入问题")  
+ 
 if p is not None:
     st.session_state["h"] +="user"+p
     st.chat_message("user").write(p)
